eb4172bb4eb74aea3fead673083abd9e22fbaf17
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2023-01-11 16:48:58 -0500

    adding contact page

---
+++ my-app/src/components/Contact/contactIndex.js
@@ -0,0 +1,120 @@
+import AnimatedLetters from '../AnimatedLetters/animatedLettersIndex'
+import './contactIndex.scss'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { useEffect, useState, useRef } from 'react'
+import emailjs from '@emailjs/browser'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
+
+    useEffect(() => {
+      let timeout;
+  
+      timeout = setTimeout(() => {
+        setLetterClass("text-animate-hover");
+      }, 3000);
+    
+      return () => {
+        clearTimeout(timeout);
+      };
+    }, []);
+
+    const sendEmail = (e) => {
+      e.preventDefault()
+
+    emailjs
+    .sendForm('gmail', 'SET UP TEMPLATE', form.current, 'your-token')
+    .then(
+      () => {
+        alert('Message successfully sent!')
+        window.location.reload(false)
+      },
+      () => {
+        alert('Failed to send the message, please try again')
+      }
+    )
+}
+
+    return (
+    <>
+    <div className='container contact-page'>
+      {/* <div className="splitting-the-page"> */}
+        <div className="text-zone">
+          <h1 className="left-sided">
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <h3 className="left-sided">
+            If you have any requests or questions, please don't hesitate to contact me.
+          </h3>
+          <div className='contact-form'>
+          <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Your name" 
+                  type="text" 
+                  name="name" 
+                  required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Your email address"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+            </div>
+          </div>
+
+
+
+        {/* <div class="right-side"> */}
+        <div className="info-map">
+            Kassi Burnett
+            <br />
+            Atlanta, Georgia
+            <br />
+            <span>burnettkassi@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={[33.75374, -84.386330]} zoom={12}>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <Marker position={[33.75374, -84.386330]}>
+              <Popup>I live here!</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      {/* </div> */}
+
+      {/* </div> */}
+    </>
+    )
+}
+
+export default Contact
