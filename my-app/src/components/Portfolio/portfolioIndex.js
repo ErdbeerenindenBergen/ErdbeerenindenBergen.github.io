b29d7a136fdadcd70222925e4d0bee397f9489c7
ErdbeerenindenBergen <114823741+ErdbeerenindenBergen@users.noreply.github.com>
2023-01-13 01:48:26 -0500

    adding photo

--- my-app/src/components/Portfolio/portfolioIndex.js
+++ my-app/src/components/Portfolio/portfolioIndex.js
@@ -24,7 +24,7 @@ const Portfolio = () => {
         {
           url: "http://erdbeerenindenbergen.github.io/taste-buds-1.jpg", 
           title: "log-in", 
-          text:"​Designed to find the perfect place to dine. Restaurant matching and voting application using MVC pattern and RESTful APIs. Registered Taste Buds users can search for restaurants in a desired area and create scheduled events. Friends are invited via unique links to view restaurant information and submit votes. The program will record votes and calculate the winning restaurants. Utilizes Java, SpringBoot, JSON, Vue.js, Yelp Fusion API, and PostgreSQL.​ Above is the log-in page.",
+          text:"Designed to find the perfect place to dine. Restaurant matching and voting application using MVC pattern and RESTful APIs. Registered Taste Buds users can search for restaurants in a desired area and create scheduled events. Friends are invited via unique links to view restaurant information and submit votes. The program will record votes and calculate the winning restaurants. Utilizes Java, SpringBoot, JSON, Vue.js, Yelp Fusion API, and PostgreSQL. Above is the log-in page.",
           subtitle: "placeholder"
         },
         {
